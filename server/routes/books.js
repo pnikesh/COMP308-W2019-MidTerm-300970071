0c6ff98d078d1409ada5da88e38252f5bcb9b6a3
Nik <nikeshpatel356@gmail.com>
2019-02-23 11:33:08 -0500

    Delete book implemented

--- server/routes/books.js
+++ server/routes/books.js
@@ -97,9 +97,18 @@ router.post("/edit/:id", (req, res, next) => {
 
 // GET - process the delete by user id
 router.get("/delete/:id", (req, res, next) => {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+  let id = req.params.id;
+
+  book.remove({_id: id}, (err) => {
+      if(err) {
+          console.log(err);
+          res.end(err);
+      }
+      else {
+          // refresh the contact list
+          res.redirect('/books');
+      }
+  });
 });
 
 module.exports = router;
