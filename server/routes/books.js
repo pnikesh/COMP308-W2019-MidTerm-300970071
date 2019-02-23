6384f3a60b06050cf80675d60ad81e7fa929c2b7
Nik <nikeshpatel356@gmail.com>
2019-02-23 10:33:51 -0500

    Add book implemented

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,70 +1,74 @@
 // modules required for routing
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
 // define the book model
-let book = require('../models/books');
+let book = require("../models/books");
 
 /* GET books List page. READ */
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // find all books in the books collection
-  book.find( (err, books) => {
+  book.find((err, books) => {
     if (err) {
       return console.error(err);
-    }
-    else {
-      res.render('books/index', {
-        title: 'Books',
+    } else {
+      res.render("books/index", {
+        title: "Books",
         books: books
       });
     }
   });
-
 });
 
 //  GET the Book Details page in order to add a new Book
-router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+router.get("/add", (req, res, next) => {
+  res.render("books/add", {
+    title: "Add New Book"
+    //displayName: req.user ? req.user.displayName : ""
+  });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
-router.post('/add', (req, res, next) => {
+router.post("/add", (req, res, next) => {
+  let newBook = book({
+    Title: req.body.Title,
+    Price: req.body.Price,
+    Author: req.body.Author,
+    Genre: req.body.Genre,
+    
+  });
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  book.create(newBook, (err, book) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      // refresh the contact list
+      res.redirect("/books");
+    }
+  });
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+router.get("/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+router.post("/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
 });
 
 // GET - process the delete by user id
-router.get('/delete/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+router.get("/delete/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
 });
-
 
 module.exports = router;
